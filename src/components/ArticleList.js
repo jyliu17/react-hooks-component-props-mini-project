0d84082bde29ca46147933353d4b1094814d996d
jyliu17 <jyliu17@gmail.com>
2021-02-01 16:26:59 -0500

    commit tree 081deb212a9e43e3b5d4196173b38c1b07cb1357

---
+++ src/components/ArticleList.js
@@ -0,0 +1,21 @@
+import Article from "./Article"
+
+function ArticleList({posts}){
+    
+    const postsList = posts.map(post => {
+        return <Article 
+                key={post.id} 
+                title={post.title} 
+                date={post.date} 
+                preview={post.preview} 
+                />
+      })
+  
+      return (
+          <main>
+              {postsList}
+          </main>
+      )
+    }
+    
+    export default ArticleList;
